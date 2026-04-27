17394038b5c6c7f01c187c457afb914fdd17bdc9
Frida Rosenaa <macrosedk@gmail.com>
2026-04-27 21:49:37 +0200

    Add icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,20 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/logo.png';
 	import { navbarItems } from '$lib/stores';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<title>10-minute love</title>
+</svelte:head>
 
 <div class="flex grow flex-col gap-2 p-2">
 	<header class="flex">
 		<nav class="flex w-full justify-between rounded-lg bg-main p-2 text-white">
-			<div class="grow">
+			<div class="flex grow">
 				<h1 class="text-2xl font-bold">10-minute love</h1>
 			</div>
 			{#if $navbarItems}
